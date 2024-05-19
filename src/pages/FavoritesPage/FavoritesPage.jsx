3ba8eba94c318b7cb4ea76c695b7894a942f89c4
Dmitriy Dudnik <84873427+Wed88@users.noreply.github.com>
2024-05-19 20:50:08 +0300

    add logic to FavoritesPage

--- src/pages/FavoritesPage/FavoritesPage.jsx
+++ src/pages/FavoritesPage/FavoritesPage.jsx
@@ -1,7 +1,27 @@
+import { CatalogItem } from '../../components/CatalogItem/CatalogItem';
+import { CamperModal } from '../../components/CamperModal/CamperModal';
+import { useSelector } from 'react-redux';
+import { selectCurrentCamper, selectFavorites } from '../../redux/selectors';
+import css from './FavoritesPage.module.css';
+
 export const FavoritesPage = () => {
+  const favorites = useSelector(selectFavorites);
+  const camper = useSelector(selectCurrentCamper);
+
   return (
-    <main>
-      <p>FavoritesPage</p>
-    </main>
+    <>
+      <div className={css.container}>
+        {favorites.length !== 0 ? (
+          <ul className={css.list}>
+            {favorites.map(camper => {
+              return <CatalogItem key={camper._id} camper={camper} />;
+            })}
+          </ul>
+        ) : (
+          'You have no favorites yet'
+        )}
+      </div>
+      {camper && <CamperModal />}
+    </>
   );
 };
