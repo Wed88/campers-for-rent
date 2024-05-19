f35293bf391f76eed9d8ae419056f2d426862d21
Dmitriy Dudnik <84873427+Wed88@users.noreply.github.com>
2024-05-19 20:11:17 +0300

    add style CamperModalForm

--- src/components/CamperModalForm/CamperModalForm.jsx
+++ src/components/CamperModalForm/CamperModalForm.jsx
@@ -1,3 +1,5 @@
+import css from './CamperModalForm.module.css';
+
 export const CamperModalForm = () => {
   const onDateFocus = evt => {
     evt.target.type = 'date';
@@ -8,13 +10,14 @@ export const CamperModalForm = () => {
   };
 
   return (
-    <form>
-      <div>
+    <form className={css.container}>
+      <div className={css.header}>
         <h2>Book your campervan now</h2>
         <p>Stay connected! We are always ready to help you.</p>
       </div>
-      <div>
+      <div className={css.fields}>
         <input
+          className={css.input}
           name="name"
           type="text"
           placeholder="Name"
@@ -22,8 +25,15 @@ export const CamperModalForm = () => {
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
         />
-        <input name="email" type="email" placeholder="Email" required />
         <input
+          className={css.input}
+          name="email"
+          type="email"
+          placeholder="Email"
+          required
+        />
+        <input
+          className={css.date}
           name="date"
           type="text"
           placeholder="Booking date"
@@ -32,9 +42,17 @@ export const CamperModalForm = () => {
           min={new Date().toISOString().split('T')[0]}
           required
         />
-        <textarea name="comment" type="text" rows={3} placeholder="Comment" />
+        <textarea
+          className={css.textarea}
+          name="comment"
+          type="text"
+          rows={3}
+          placeholder="Comment"
+        />
       </div>
-      <button type="submit">Send</button>
+      <button className={css.button} type="submit">
+        Send
+      </button>
     </form>
   );
 };
