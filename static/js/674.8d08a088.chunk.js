8b813c365d7d1688ede128419bcd3743184fc291
Wed88 <84873427+Wed88@users.noreply.github.com>
2024-05-20 19:00:37 +0000

    Deploying to gh-pages from @ Wed88/campers-for-rent@6eabd6216e8add437b608e94ad3d8141fbe08997 🚀

---
+++ static/js/674.8d08a088.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcampers_for_rent=self.webpackChunkcampers_for_rent||[]).push([[674],{8674:function(e,r,a){a.r(r),a.d(r,{HomePage:function(){return o}});var s="HomePage_container__uNu-e",n="HomePage_title__UFLA3",c="HomePage_text__2XixT",t=a(184),o=function(){return(0,t.jsx)("div",{className:s,children:(0,t.jsxs)("div",{className:n,children:[(0,t.jsx)("h1",{className:c,children:"Campers Rent"}),(0,t.jsx)("p",{className:c,children:"choose your camper and travel in comfort"})]})})}}}]);
+//# sourceMappingURL=674.8d08a088.chunk.js.map
